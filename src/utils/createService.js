8496f97a572797f51a4ab2ec0355a9499d9247ef
Zyao89 <zyao89@gmail.com>
2020-03-23 17:35:13 +0800

    chore: update deps

--- src/utils/createService.js
+++ src/utils/createService.js
@@ -1,10 +1,10 @@
 'use strict';
 
-const Service = require('@micro-app/core');
+const { createService } = require('@micro-app/cli');
 const { SKIP_TARGET, BUILT_IN } = require('../constants');
 
 module.exports = function() {
-    const service = new Service({ target: SKIP_TARGET });
+    const service = createService({ target: SKIP_TARGET });
 
     const WEBPACK_PLUGIN_ID = '@micro-app/plugin-webpack';
     if (!service.hasPlugin(WEBPACK_PLUGIN_ID)) {

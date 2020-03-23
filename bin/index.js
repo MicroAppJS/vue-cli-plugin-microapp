afa28d72ff769f8b81f7ac090fad85cb8e86e5b9
Zyao89 <zyao89@gmail.com>
2020-03-23 16:36:12 +0800

    chore: update scripts

---
+++ bin/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const { yParser } = require('@micro-app/shared-utils');
+const name = process.argv[2];
+const argv = yParser(process.argv.slice(3));
+
+const Service = require('@micro-app/core');
+const server = new Service();
+
+console.log(argv);
+server.run(name, argv);

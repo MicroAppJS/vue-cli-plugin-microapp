2850768f87fa587ad8f03a006c2a1117836a5e2b
Zyao89 <zyao89@gmail.com>
2020-03-31 12:22:39 +0800

    chore: update deps

--- src/utils/registerMethod.js
+++ src/utils/registerMethod.js
@@ -1,16 +1,18 @@
 'use strict';
 
-const { assert } = require('@micro-app/shared-utils');
+const { assert, logger } = require('@micro-app/shared-utils');
 const { CONSTANTS } = require('@micro-app/core');
 const KEY = 'modifyVueConfig';
 
 // 注册全局, hacker, 外部调用
 module.exports = function registerMethod(s) {
-    assert(s, 'service must be required!');
+    assert(s, 'service or api must be required!');
     if (!s.hasKey(KEY)) {
         s.registerMethod(KEY, {
             type: CONSTANTS.API_TYPE.MODIFY,
             description: 'modify vue config.',
         });
+    } else {
+        logger.warn(`[${KEY}]`, 'has exists.');
     }
 };
